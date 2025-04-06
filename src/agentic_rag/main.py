f042e49c050be994dbbf60410ac85af81df608a2
Sourangshu Pal <paulbindass@gmail.com>
2025-04-06 15:00:06 +0530

    minor updates

--- src/agentic_rag/main.py
+++ src/agentic_rag/main.py
@@ -9,10 +9,6 @@ from agentic_rag.crew import AgenticRag
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
 
 def run():
     """
